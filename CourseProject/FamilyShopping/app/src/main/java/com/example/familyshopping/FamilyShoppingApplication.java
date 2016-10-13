c2946dc0e3ffc10c892efaa45f2a22811c1bb032
TanyaRan <tatyanaran@gmail.com>
2016-10-13 18:23:58 +0300

    all ui

---
+++ CourseProject/FamilyShopping/app/src/main/java/com/example/familyshopping/FamilyShoppingApplication.java
@@ -0,0 +1,16 @@
+package com.example.familyshopping;
+
+import android.app.Application;
+
+/**
+ * Includes one-time initialization of Farebase
+ */
+
+public class FamilyShoppingApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
---
+++ CourseProject/FamilyShopping/app/src/main/java/com/example/familyshopping/utils/Constants.java
@@ -0,0 +1,13 @@
+package com.example.familyshopping.utils;
+
+/**
+ * Constants class store most important strings and paths of the app
+ */
+
+public final class Constants {
+
+    /**
+     * Constants for Firebase URL
+     */
+    //public static final String FIREBASE_URL = BuildConfig.UNIQUE_FIREBASE_ROOT_URL;
+}
